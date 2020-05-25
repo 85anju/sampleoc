9bb98ae2d146adf239be523c9f5ea47d3e0c8f1d
85anju <37234538+85anju@users.noreply.github.com>
2020-05-25 12:30:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '''<h1> you've reached the home page </h1>'''
+  return '''<h1> python app--dockerfile</h1>'''
 
 app.run(host='0.0.0.0',port=8080)
