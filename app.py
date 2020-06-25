c47f1d12c31f77c413ea9643e65b33ac40ef6e67
85anju <37234538+85anju@users.noreply.github.com>
2020-06-25 10:05:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-  return '''<h1> python app--dockerfile</h1>'''
-
-app.run(host='0.0.0.0',port=8080)
+print("hiiiii")
